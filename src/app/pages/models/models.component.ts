94108865b58971c06c81c1ea69267f37489ad6ec
joanpuigra <joanpuigra@campus.monlau.com>
2024-05-06 16:40:40 +0200

    All elements working

--- src/app/pages/models/models.component.ts
+++ src/app/pages/models/models.component.ts
@@ -1,22 +1,28 @@
-import {Component, OnInit} from '@angular/core';
-import {FormDataService} from '../../services/formData.service';
-import {RouterLink} from "@angular/router";
+import { Component, OnInit } from "@angular/core";
+import { FormDataService } from "../../services/formData.service";
+import { RouterLink } from "@angular/router";
+import { NgForOf, NgIf } from "@angular/common";
 
 @Component({
-    selector: 'app-models',
-    standalone: true,
-    imports: [RouterLink],
-    templateUrl: './models.component.html',
-    styleUrl: './models.component.css'
+  selector: "app-models",
+  standalone: true,
+  imports: [RouterLink, NgIf, NgForOf],
+  templateUrl: "./models.component.html",
+  styleUrl: "./models.component.css",
 })
-
 export class ModelsComponent implements OnInit {
-    formData: any;
+  formData: any = [];
 
-    constructor(private formDataService: FormDataService) {}
+  constructor(private formDataService: FormDataService) {}
 
-    ngOnInit(): void {
-        this.formData = this.formDataService.getData();
-        console.log(this.formData);
+  ngOnInit(): void {
+    const currentData = this.formDataService.getData();
+    if (!currentData || currentData.length === 0) {
+      console.log("No data found");
+      return;
+    } else {
+      console.log("Values: ", currentData);
+      this.formData = currentData;
     }
+  }
 }
